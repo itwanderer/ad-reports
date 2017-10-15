585736095e14b94efa4679a679795b76a46eaf3a
arden <ermitanyo@gmail.com>
2017-10-15 00:27:09 -0700

    Initial Commit
    Initial version for generating information on direct and all reports based on the directReports attribute in Active DIrectory

---
+++ ad-reports.ps1
@@ -0,0 +1,79 @@
+Function Generate-Reports {
+	Param(
+		[String]$ManagerDN,
+		[String]$SearchBase,
+		[Switch]$Recursive
+	
+	)
+    $LdapFilter = "(&(objectclass=user)(manager=" + $managerDN + "))"
+	$DirectReports = Get-ADUser -LDAPFilter $LdapFilter -pro directReports
+    if ($DirectReports -eq $null) { continue; }
+    $AllReports += $DirectReports
+
+    If ($recursive) {
+
+        ForEach ($entry in $DirectReports) {
+             If ($entry.DirectReports -gt 0) {
+                If ($entry.distinguishedname -eq $managerDN) {
+                    Write-Host "Circular manager reference, skipping $($entry.name)"
+                    Continue
+                }
+                Generate-Reports -managerDN $entry.distinguishedname  -recursive
+            }
+        
+        }
+    }
+
+    return $AllReports
+}
+
+
+
+
+Function Process-Reports {
+    Param(
+        $List,
+        $Group
+    )
+    
+    $MemberHash = @{}
+    $DisabledMembers = $null
+    $EnabledMembers = $null
+
+    $ReportState = $List | Group-Object Enabled| Sort-Object Name -Descending
+    Write-Host "`n$($Group)"
+    Write-Host "    Total Members: $($List.sAMAccountName.Count)"
+    ForEach ($State in $ReportState) {
+        If ($State.name -eq "True") { 
+            Write-Host "      Enabled Members: $($State.Count)"
+            $EnabledMembers = $State.group
+            $MemberHash.Enabled = $EnabledMembers
+        }
+        Else {
+            Write-Host "      Disabled Members: $($State.Count)"
+            $DisabledMembers = $State.group
+            $MemberHash.Disabled = $DisabledMembers
+        }
+        
+    }
+    return $MemberHash
+
+}
+
+$Managers = Get-ADUser -LDAPFilter "(&(objectclass=user)(directreports=*))"
+
+ForEach ($Mgr in $Managers) {
+
+    $drMemberHash = @{}    
+    $DirectReports = Generate-Reports -ManagerDN $Mgr.DistinguishedName
+    $drMemberHash = Process-Reports -List $DirectReports -Group "$($Mgr.SamAccountName)-DirectReports"
+    
+    
+    $arMemberHash = @{}
+    $AllReports = Generate-Reports -ManagerDN $Mgr.DistinguishedName -Recursive
+    $arMemberHash = Process-Reports -List $AllReports -Group "$($Mgr.SamAccountName)-AllReports"
+    #use $hash.ContainsKey("key) to check for key existence since it is possible that only one key exists
+    #use $hash.Get_Item("key") to get value for associated key
+    
+}
+
